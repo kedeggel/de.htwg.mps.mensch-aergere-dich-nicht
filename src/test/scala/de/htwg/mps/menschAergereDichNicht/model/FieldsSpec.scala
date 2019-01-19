ffcb445dd0fcfda19722f94676ba059764338f57
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2019-01-19 17:44:18 +0100

    removed unused test

--- src/test/scala/de/htwg/mps/menschAergereDichNicht/model/FieldsSpec.scala
+++ src/test/scala/de/htwg/mps/menschAergereDichNicht/model/FieldsSpec.scala
@@ -3,18 +3,6 @@ package de.htwg.mps.menschAergereDichNicht.model
 import org.scalatest.{Matchers, WordSpec}
 
 class FieldsSpec extends WordSpec with Matchers {
-//  "A field" should {
-//    val field = NormalField(1)
-//    "be not occupied without a peg on it" in {
-//      field.isOccupied should be(false)
-//    }
-//
-//    "be occupied with a peg on it" in {
-//      field.peg = Some(Peg(Color.yellow, field))
-//      field.isOccupied should be(true)
-//    }
-//  }
-
   "A normal field" should {
     val field = NormalField(1)
     "be represented with \"o\" if empty" in {
